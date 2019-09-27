b539333e2f02c9a87d8783ef34ee27c0da34bbb3
Anna Myzukina <anna.muzykina83@gmail.com>
2019-09-27 12:22:18 +0300

    add books database

---
+++ books.sql
@@ -0,0 +1,6 @@
+CREATE TABLE books (id INTEGER PRIMARY KEY, name TEXT, rating INTEGER );
+
+INSERT INTO books VALUES (1, "First", 4);
+INSERT INTO books VALUES (2, "Second", 1);
+INSERT INTO books VALUES (3, "Third", 2);
+SELECT * FROM books
