719a23e9d5553b6d63de99a541f9aac0c3f8aa00
Anna Myzukina <anna.muzykina83@gmail.com>
2019-09-27 12:31:09 +0300

    create flowers data base

---
+++ flowers.sql
@@ -0,0 +1,6 @@
+CREATE TABLE flowers (id INTEGER PRIMARY KEY, name TEXT, flowering TEXT, colours TEXT, size INTEGER);
+INSERT INTO flowers VALUES (1, "Rose", "june", "red", 30);
+INSERT INTO flowers VALUES (2, "Lilija", "may", "yelow", 40);
+INSERT INTO flowers VALUES (3, "Erimurus", "august", "pink", 80);
+INSERT INTO flowers VALUES (4, "Esholcija", "april", "orange", 20);
+SELECT * FROM flowers;
